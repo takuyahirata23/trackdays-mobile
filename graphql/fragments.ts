f73f0a7264c6cb459940564af84544414c7c2b1e
Takuya Hirata <takuyahirata4@gmail.com>
2023-08-25 08:47:59 -0400

    Feat: save trackday

--- graphql/fragments.ts
+++ graphql/fragments.ts
@@ -14,3 +14,29 @@ export const MOTORCYCLE = gql`
     }
   }
 `
+
+export const TRACKDAY = gql`
+  fragment NewTrackday on Trackdays {
+    id
+    date
+    track {
+      id
+      facility {
+        id
+        name
+      }
+    }
+    motorcycle {
+      id
+      year
+      model {
+        id
+        name
+        make {
+          id
+          name
+        }
+      }
+    }
+  }
+`
--- graphql/mutations.ts
+++ graphql/mutations.ts
@@ -18,3 +18,31 @@ export const REGISTER_MOTORCYCLE = gql`
     }
   }
 `
+
+export const SAVE_TRACKDAY = gql`
+  mutation saveTrackday($saveTrackdayInput: SaveTrackdayInput!) {
+    saveTrackday(saveTrackdayInput: $saveTrackdayInput) {
+      id
+      date
+      track {
+        id
+        facility {
+          id
+          name
+        }
+      }
+      motorcycle {
+        id
+        year
+        model {
+          id
+          name
+          make {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+`
