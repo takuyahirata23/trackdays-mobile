5986a97beb3d89dfc1a988ec37aac41fae47d6ac
Takuya Hirata <takuyahirata4@gmail.com>
2023-10-04 20:22:03 -0400

    Refactore: fetch user

--- context/Auth.tsx
+++ context/Auth.tsx
@@ -16,6 +16,14 @@ type RegisterFields = {
   name: string
 } & SignInFields
 
+type Reponse = {
+  message: string
+  errors?: { [key: string]: string[] }
+  error: boolean
+  user: User
+  token: string
+}
+
 type Props = {
   children: React.ReactNode
   setUser: (user: User | null) => void
@@ -24,6 +32,12 @@ type Props = {
 type Error = {
   message?: string
   fields?: { [key: string]: string[] }
+}
+
+type ErrorsFromAPI = {
+  message: string
+  errors?: { [key: string]: string[] }
+  error: boolean
 }
 
 type AuthContextType = {
@@ -40,6 +54,7 @@ export const AuthContext = React.createContext<AuthContextType>({
   error: null
 })
 
+
 const base = `${process.env.DOMAIN_URL}/auth/`
 const loginPath = base.concat('login')
 const registerPath = base.concat('register')
@@ -51,28 +66,29 @@ const fetchUser = (path: string, body: SignInFields) =>
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(body)
-  })
+  }).then(x => x.json())
 
 export function AuthProvider({ children, setUser }: Props) {
   const [error, setError] = React.useState<null | Error>(null)
 
+  const handleErrorFromAPI = (data: ErrorsFromAPI) => {
+    setError({
+      message: data.message,
+      fields: data.errors
+    })
+  }
+
+  const handleUserResponse = (d: { token: string; user: User }) => {
+    saveToken(d.token)
+    setError(null)
+    setUser(d.user)
+  }
+
+  const handleResponse = (d: Reponse) => d.error ? handleErrorFromAPI(d) : handleUserResponse(d)
+
   const signIn = (body: SignInFields) =>
     fetchUser(loginPath, body)
-      .then(x => {
-        return x.json()
-      })
-      .then(d => {
-        if (d.error) {
-          setError({
-            message: d.message,
-            fields: d.errors
-          })
-        } else {
-          saveToken(d.token)
-          setError(null)
-          setUser(d.user)
-        }
-      })
+      .then(handleResponse)
       .catch(e => {
         console.error(e)
         setError({ message: 'User not found. Please try again.' })
@@ -85,21 +101,7 @@ export function AuthProvider({ children, setUser }: Props) {
 
   const register = (body: RegisterFields) =>
     fetchUser(registerPath, body)
-      .then(x => {
-        return x.json()
-      })
-      .then(d => {
-        if (!d.error) {
-          saveToken(d.token)
-          setError(null)
-          setUser(d.user)
-        } else {
-          setError({
-            message: d.message,
-            fields: d.errors
-          })
-        }
-      })
+      .then(handleResponse)
       .catch(e => {
         console.log(e)
         setError({ message: 'Registration failed. Please try again.' })
