91f7fca1379c20bddc44d8677c2e963dc10580e6
Takuya Hirata <takuyahirata4@gmail.com>
2023-11-27 18:05:49 -0500

    Refactor: bottom sheet based save note

--- app/(tabs)/trackday/note.tsx
+++ app/(tabs)/trackday/note.tsx
@@ -5,6 +5,7 @@ import { useQuery, useLazyQuery, useMutation } from '@apollo/client'
 import { Picker } from '@react-native-picker/picker'
 import { useSearchParams } from 'expo-router'
 import { isEmpty } from 'ramda'
+import BottomSheet from '@gorhom/bottom-sheet'
 
 import {
   FACILITIES_QUERY,
@@ -13,7 +14,15 @@ import {
 } from '@graphql/queries'
 import { SAVE_TRACKDAY_NOTE } from 'graphql/mutations'
 import { TRACKDAY_NOTE } from 'graphql/fragments'
-import { Button, Card, Field, Text, Container } from '@components'
+import {
+  Button,
+  Card,
+  Field,
+  Text,
+  Container,
+  BottomSheetHandle,
+  KeyboardAvoidingView
+} from '@components'
 import {
   minutesToMilliseconds,
   secondsToMilliseconds
@@ -45,30 +54,31 @@ const goBackToPreviousStep = (prev: SaveTrackdaySteps) => {
 
     case SaveTrackdaySteps.Laptime:
       return SaveTrackdaySteps.Motorcycle
-
-    case SaveTrackdaySteps.Note:
-      return SaveTrackdaySteps.Laptime
-
-    case SaveTrackdaySteps.Submit:
-      return SaveTrackdaySteps.Note
   }
 }
 
 export default function CreateTrackdayNote() {
-  const { date } = useSearchParams()
+  const bottomSheetRef = React.useRef<BottomSheet>(null)
+  const { date: dateParam } = useSearchParams()
   const { goBack } = useNavigation()
   const motorcycleRes = useQuery(MOTORCYCLES_QUERY)
   const facilityRes = useQuery(FACILITIES_QUERY)
   const [currentStep, setCurrentStep] = React.useState(
     SaveTrackdaySteps.Facility
   )
-  const [motorcycle, setMotorcycle] = React.useState('')
-  const [facility, setFacility] = React.useState('')
-  const [track, setTrack] = React.useState('')
-  const [minutes, setMinutes] = React.useState('')
-  const [seconds, setSeconds] = React.useState('')
-  const [milliseconds, setMilliseconds] = React.useState('')
-  const [note, setNote] = React.useState('')
+  const [
+    { date, facility, track, motorcycle, minutes, seconds, milliseconds, note },
+    setFields
+  ] = React.useState({
+    date: dateParam,
+    facility: '',
+    track: '',
+    motorcycle: '',
+    minutes: '',
+    seconds: '',
+    milliseconds: '',
+    note: ''
+  })
   const [getTracks, tracksRes] = useLazyQuery(TRACKS_QUERY, {
     variables: {
       facilityId: facility
@@ -96,6 +106,9 @@ export default function CreateTrackdayNote() {
         }
       })
     },
+    onError(e) {
+      console.log('error', e)
+    },
     onCompleted() {
       goBack()
     }
@@ -112,46 +125,32 @@ export default function CreateTrackdayNote() {
     secondsToMilliseconds(Number(seconds)) +
     Number(milliseconds)
 
+  const handleOnChange = (field: string) => (value: string) =>
+    setFields(prev => ({ ...prev, [field]: value }))
+
   const onPressHandlers = () => {
     switch (currentStep) {
       case SaveTrackdaySteps.Facility:
         return () => {
           if (!facility) {
-            setFacility(facilityRes.data.facilities[0].id)
+            handleOnChange('facility')(facilityRes.data.facilities[0].id)
           }
           setCurrentStep(SaveTrackdaySteps.Track)
         }
       case SaveTrackdaySteps.Track:
         return () => {
           if (!track) {
-            setTrack(tracksRes.data.tracks[0].id)
+            handleOnChange('track')(tracksRes.data.tracks[0].id)
           }
           setCurrentStep(SaveTrackdaySteps.Motorcycle)
         }
       case SaveTrackdaySteps.Motorcycle:
         return () => {
           if (!motorcycle) {
-            setMotorcycle(motorcycleRes.data.motorcycles[0].id)
+            handleOnChange('motorcycle')(motorcycleRes.data.motorcycles[0].id)
           }
           setCurrentStep(SaveTrackdaySteps.Laptime)
-        }
-      case SaveTrackdaySteps.Laptime:
-        return () => setCurrentStep(SaveTrackdaySteps.Note)
-      case SaveTrackdaySteps.Note:
-        return () => setCurrentStep(SaveTrackdaySteps.Submit)
-      case SaveTrackdaySteps.Submit:
-        return () => {
-          saveTrackdayNote({
-            variables: {
-              saveTrackdayNoteInput: {
-                date,
-                lapTime: laptimeToMilliseconds(),
-                motorcycleId: motorcycle,
-                trackId: track,
-                note: note
-              }
-            }
-          })
+          bottomSheetRef.current?.close()
         }
     }
   }
@@ -163,147 +162,189 @@ export default function CreateTrackdayNote() {
   const getMotorcycleName = (data: any) => (id: string) =>
     data.find((x: any) => x.id === id)?.model.name || ''
 
+  const handleSubmit = () =>
+    saveTrackdayNote({
+      variables: {
+        saveTrackdayNoteInput: {
+          date,
+          lapTime: laptimeToMilliseconds(),
+          motorcycleId: motorcycle,
+          trackId: track,
+          note: note
+        }
+      }
+    })
+
   return (
-    <Container style={styles.container}>
+    <Container>
       {motorcycleRes.called &&
       !motorcycleRes.loading &&
       isEmpty(motorcycleRes.data.motorcycles) ? (
         <NoMotorcycleRegisteredError />
       ) : (
         <>
-          <Card>
-            <Text>Date: {date}</Text>
-            <Text>Track: {getName(tracksRes.data?.tracks || [])(track)}</Text>
-            <Text>
-              Motorcycle:{' '}
-              {getMotorcycleName(motorcycleRes.data?.motorcycles || [])(
-                motorcycle
+          <KeyboardAvoidingView>
+            <View style={styles.container}>
+              <Card>
+                <Text>Date: {date}</Text>
+              </Card>
+              <Card>
+                <Text>
+                  Facility: {getName(facilityRes.data?.facilities || [])(facility)}
+                </Text>
+              </Card>
+              <Card>
+                <Text>
+                  Track: {getName(tracksRes.data?.tracks || [])(track)}
+                </Text>
+              </Card>
+              <Card>
+                <Text>
+                  Motorcycle:{' '}
+                  {getMotorcycleName(motorcycleRes.data?.motorcycles || [])(
+                    motorcycle
+                  )}
+                </Text>
+              </Card>
+              {currentStep === SaveTrackdaySteps.Laptime ? (
+                <Card>
+                  <View style={styles.laptimeWrapper}>
+                    <View style={styles.lapTimeFieldWrapper}>
+                      <Field
+                        label="Minute"
+                        value={minutes}
+                        style={styles.lapTimeField}
+                        onChangeText={handleOnChange('minutes')}
+                        keyboardType="numeric"
+                        placeholder="00"
+                      />
+                      <Text style={styles.laptimeSemicolon}>:</Text>
+                    </View>
+                    <View style={styles.lapTimeFieldWrapper}>
+                      <Field
+                        label="Seconds"
+                        value={seconds}
+                        onChangeText={handleOnChange('seconds')}
+                        style={styles.lapTimeField}
+                        keyboardType="numeric"
+                        placeholder="00"
+                      />
+                      <Text style={styles.laptimeSemicolon}>:</Text>
+                    </View>
+                    <View style={styles.lapTimeFieldWrapper}>
+                      <Field
+                        label="Miliseconds"
+                        value={milliseconds}
+                        onChangeText={handleOnChange('milliseconds')}
+                        style={styles.lapTimeField}
+                        keyboardType="numeric"
+                        placeholder="000"
+                        returnKeyType="done"
+                        onEndEditing={() => {
+                          setCurrentStep(SaveTrackdaySteps.Note)
+                        }}
+                      />
+                    </View>
+                  </View>
+                </Card>
+              ) : (
+                <Card>
+                  <Text>
+                    Best lap time: {minutes || '00'}:{seconds || '00'}:
+                    {milliseconds || '000'}
+                  </Text>
+                </Card>
               )}
-            </Text>
-            <Text>
-              Best lap time: {minutes || '00'}:{seconds || '00'}:
-              {milliseconds || '000'}
-            </Text>
-            {currentStep === SaveTrackdaySteps.Submit && <Text>{note}</Text>}
-          </Card>
-          {currentStep === SaveTrackdaySteps.Facility && (
-            <Card>
-              <View style={styles.pickerWrapper}>
-                <Picker selectedValue={facility} onValueChange={setFacility}>
-                  {facilityRes?.data?.facilities.map((m: Facility) => (
-                    <Picker.Item value={m.id} label={m.name} key={m.id} />
-                  ))}
-                </Picker>
+              {currentStep === SaveTrackdaySteps.Note ? (
+                <Card>
+                  <Field
+                    label="Note"
+                    value={note}
+                    onChangeText={handleOnChange('note')}
+                    numberOfLines={3}
+                    inputStyle={styles.note}
+                    multiline
+                    blurOnSubmit
+                    onSubmitEditing={() => {
+                      setCurrentStep(SaveTrackdaySteps.Submit)
+                      Keyboard.dismiss()
+                    }}
+                    returnKeyType="done"
+                  />
+                </Card>
+              ) : (
+                <Card>
+                  <Text>Note: {note}</Text>
+                </Card>
+              )}
+              <View style={styles.btnWrapper}>
+                <Button onPress={handleSubmit}>Save</Button>
               </View>
-            </Card>
-          )}
-          {currentStep === SaveTrackdaySteps.Track && (
-            <Card>
-              {facility && tracksRes.data && (
+            </View>
+          </KeyboardAvoidingView>
+          <BottomSheet
+            ref={bottomSheetRef}
+            snapPoints={['40%']}
+            handleComponent={() => (
+              <BottomSheetHandle
+                onPressRight={onPressHandlers()}
+                rightText="Next"
+                onPressLeft={() => setCurrentStep(goBackToPreviousStep)}
+                leftText={currentStep !== SaveTrackdaySteps.Facility && 'Back'}
+              />
+            )}
+          >
+            {currentStep === SaveTrackdaySteps.Facility && (
+              <Card>
                 <View style={styles.pickerWrapper}>
                   <Picker
-                    selectedValue={track}
-                    onValueChange={v => {
-                      console.log(v)
-                      setTrack(v)
-                    }}
+                    selectedValue={facility}
+                    onValueChange={handleOnChange('facility')}
                   >
-                    {tracksRes?.data?.tracks?.map((m: Track) => (
+                    {facilityRes?.data?.facilities.map((m: Facility) => (
                       <Picker.Item value={m.id} label={m.name} key={m.id} />
                     ))}
                   </Picker>
                 </View>
-              )}
-            </Card>
-          )}
-          {currentStep === SaveTrackdaySteps.Motorcycle && (
-            <Card>
-              <View style={styles.pickerWrapper}>
-                <Picker
-                  selectedValue={motorcycle}
-                  onValueChange={setMotorcycle}
-                >
-                  {motorcycleRes?.data?.motorcycles.map(
-                    ({ id, year, model }: Motorcycle) => (
-                      <Picker.Item
-                        value={id}
-                        label={`${model.name}(${year})`}
-                        key={id}
-                      />
-                    )
-                  )}
-                </Picker>
-              </View>
-            </Card>
-          )}
-          {currentStep === SaveTrackdaySteps.Laptime && (
-            <Card>
-              <View style={styles.laptimeWrapper}>
-                <View style={styles.lapTimeFieldWrapper}>
-                  <Field
-                    label="Minute"
-                    value={minutes}
-                    style={styles.lapTimeField}
-                    onChangeText={setMinutes}
-                    keyboardType="numeric"
-                    placeholder="00"
-                  />
-                  <Text style={styles.laptimeSemicolon}>:</Text>
-                </View>
-                <View style={styles.lapTimeFieldWrapper}>
-                  <Field
-                    label="Seconds"
-                    value={seconds}
-                    onChangeText={setSeconds}
-                    style={styles.lapTimeField}
-                    keyboardType="numeric"
-                    placeholder="00"
-                  />
-                  <Text style={styles.laptimeSemicolon}>:</Text>
-                </View>
-                <View style={styles.lapTimeFieldWrapper}>
-                  <Field
-                    label="Miliseconds"
-                    value={milliseconds}
-                    onChangeText={setMilliseconds}
-                    style={styles.lapTimeField}
-                    keyboardType="numeric"
-                    placeholder="000"
-                    returnKeyType="done"
-                  />
-                </View>
-              </View>
-            </Card>
-          )}
-          {currentStep === SaveTrackdaySteps.Note && (
-            <Card>
-              <Field
-                label="Note"
-                value={note}
-                onChangeText={setNote}
-                numberOfLines={3}
-                inputStyle={styles.note}
-                multiline
-                blurOnSubmit
-                onSubmitEditing={() => Keyboard.dismiss()}
-                returnKeyType="done"
-              />
-            </Card>
-          )}
-          <View style={styles.btnWrapper}>
-            <Button onPress={onPressHandlers()}>
-              {currentStep === SaveTrackdaySteps.Submit ? 'Save' : 'Next'}
-            </Button>
-            {currentStep !== SaveTrackdaySteps.Facility && (
-              <Button
-                // @ts-ignore
-                onPress={() => setCurrentStep(goBackToPreviousStep)}
-                variant="secondary"
-              >
-                Back
-              </Button>
+              </Card>
             )}
-          </View>
+            {currentStep === SaveTrackdaySteps.Track && (
+              <Card>
+                {facility && tracksRes.data && (
+                  <View style={styles.pickerWrapper}>
+                    <Picker
+                      selectedValue={track}
+                      onValueChange={handleOnChange('track')}
+                    >
+                      {tracksRes?.data?.tracks?.map((m: Track) => (
+                        <Picker.Item value={m.id} label={m.name} key={m.id} />
+                      ))}
+                    </Picker>
+                  </View>
+                )}
+              </Card>
+            )}
+            {currentStep === SaveTrackdaySteps.Motorcycle && (
+              <Card>
+                <View style={styles.pickerWrapper}>
+                  <Picker
+                    selectedValue={motorcycle}
+                    onValueChange={handleOnChange('motorcycle')}
+                  >
+                    {motorcycleRes?.data?.motorcycles.map(
+                      ({ id, year, model }: Motorcycle) => (
+                        <Picker.Item
+                          value={id}
+                          label={`${model.name}(${year})`}
+                          key={id}
+                        />
+                      )
+                    )}
+                  </Picker>
+                </View>
+              </Card>
+            )}
+          </BottomSheet>
         </>
       )}
     </Container>
@@ -312,7 +353,9 @@ export default function CreateTrackdayNote() {
 
 const styles = StyleSheet.create({
   container: {
-    rowGap: 16
+    rowGap: 16,
+    flex: 1,
+    paddingBottom: 16
   },
   laptimeWrapper: {
     width: '100%',
