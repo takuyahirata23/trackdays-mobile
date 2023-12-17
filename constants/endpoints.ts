29c782978b1123d8be6364e6137e7e74de7e6368
Takuya Hirata <takuyahirata4@gmail.com>
2023-12-17 18:37:14 -0500

    Config: ESA build config

--- constants/endpoints.ts
+++ constants/endpoints.ts
@@ -1,7 +1,6 @@
-const BASE = `${process.env.DOMAIN_URL}`
+const BASE = `${process.env.EXPO_PUBLIC_DOMAIN_URL}`
 
 export const AUTH = BASE.concat('/auth')
-
 export const LOGIN = AUTH.concat('/login')
 export const REGISTER = AUTH.concat('/register')
 export const DELETE_ACCOUNT = AUTH.concat('/delete-account')
