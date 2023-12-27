9666c94452e596e099f83908c7a402768d6e659f
Takuya Hirata <takuyahirata4@gmail.com>
2023-12-27 10:19:12 -0500

    Chore: make status bar dark regardless user default theme

--- components/Container.tsx
+++ components/Container.tsx
@@ -23,6 +23,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 20,
-    paddingTop: 20
+    paddingTop: 20,
+    // borderTopWidth: 1,
+    // borderBottomWidth: 1
   }
 })
