3bb32153fa9394ed66401076e27d9e1633c52c92
Takuya Hirata <takuyahirata4@gmail.com>
2024-01-22 18:58:28 -0500

    Refactor: use scrollview for facilitiy list

--- app/(tabs)/track/index.tsx
+++ app/(tabs)/track/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { StyleSheet, TouchableOpacity } from 'react-native'
+import { StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
 import { useQuery } from '@apollo/client'
 import { Link } from 'expo-router'
+import { Toast } from 'toastify-react-native'
 
-import { Container, Card, Text } from '@components'
+import { Container, Card, Text, ActivityIndicator } from '@components'
 import { FACILITIES_QUERY } from '@graphql/queries'
 
 import type { Facility } from '@type/park'
@@ -12,42 +13,55 @@ export default function Leaderboard() {
   const { loading, data, error } = useQuery(FACILITIES_QUERY)
 
   if (loading) {
-    return null
+    return <ActivityIndicator />
   }
 
   if (error) {
+    Toast.error('Sorry! Something went wrong.', 'bottom')
     return null
   }
 
   return (
     <Container style={styles.container}>
-      {data.facilities?.map(({ id, name, description }: Facility) => (
-        <Link
-          href={{ pathname: '/track/[id]', params: { id } }}
-          key={id}
-          asChild
-        >
-          <TouchableOpacity>
-            <Card key={id} style={styles.card}>
-              <Text style={styles.facility}>{name}</Text>
-              <Text
-                style={styles.description}
-                ellipsizeMode="tail"
-                numberOfLines={2}
-              >
-                {description}
-              </Text>
-            </Card>
-          </TouchableOpacity>
-        </Link>
-      ))}
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        showsVerticalScrollIndicator={false}
+      >
+        {data.facilities?.map(({ id, name, description }: Facility) => (
+          <Link
+            href={{ pathname: '/track/[id]', params: { id } }}
+            key={id}
+            asChild
+          >
+            <TouchableOpacity>
+              <Card key={id} style={styles.card}>
+                <Text style={styles.facility}>{name}</Text>
+                <Text
+                  style={styles.description}
+                  ellipsizeMode="tail"
+                  numberOfLines={2}
+                >
+                  {description}
+                </Text>
+              </Card>
+            </TouchableOpacity>
+          </Link>
+        ))}
+      </ScrollView>
     </Container>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    rowGap: 16
+    paddingTop: 0,
+    paddingHorizontal: 0
+  },
+  scrollView: {
+    marginTop: 16,
+    paddingBottom: 32,
+    rowGap: 16,
+    paddingHorizontal: 20
   },
   card: {
     rowGap: 8
