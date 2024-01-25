29ed498cb0921713c71fe3084866322fb3369ecc
Takuya Hirata <takuyahirata4@gmail.com>
2024-01-24 21:14:45 -0500

    Test: Field

--- components/__tests__/Field.test.tsx
+++ components/__tests__/Field.test.tsx
@@ -3,11 +3,18 @@ import { render, screen } from '@testing-library/react-native'
 
 import { Field } from '../Field'
 
-describe('handle error', () => {
+describe('Display error', () => {
   test('should render error message', () => {
     render(<Field label="Text" onChangeText={() => null} error="Error" />)
     const res = screen.getByText('Error')
 
     expect(res).toBeOnTheScreen()
+  })
+
+  test('should multiple errors when error is array', () => {
+    const errors = ['E1', 'E2', 'Last']
+    render(<Field label="Text" onChangeText={() => null} error={errors} />)
+
+    errors.map(e => expect(screen.getByText(e)).toBeOnTheScreen())
   })
 })
